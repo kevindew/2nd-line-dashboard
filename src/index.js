d77898aebf79310fcb7ec516bb4a4e3421c602f2
Kevin Dew <kevindew@me.com>
2018-04-20 21:05:05 +0100

    Install Redux

--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers';
 import './index.css';
 import Dashboard from './Dashboard';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Dashboard />, document.getElementById('root'));
+const store = createStore(rootReducer);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Dashboard />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
---
+++ src/reducers/index.js
@@ -0,0 +1,5 @@
+const initialState = {}
+
+export default function rootReducer(state = initialState) {
+  return state;
+}
